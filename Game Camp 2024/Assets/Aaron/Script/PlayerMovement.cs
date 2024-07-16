fdc4112a7b50c0d4d384c3802a11da979468a5a0
Aaron Hartsel <aaron.hartsel@gmail.com>
2024-07-16 17:35:24 -0500

    Updated WhiteBox and Made the Exit Gate

--- Game Camp 2024/Assets/Aaron/Script/PlayerMovement.cs
+++ Game Camp 2024/Assets/Aaron/Script/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D controller;
-
+    public NPCCounter counter;
     public float walkSpeed = 25f;
   
     Vector2 movement;
@@ -13,9 +13,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
-    
+        
+        //walkSpeed = counter.speed;
+        Debug.Log("Current Speed: " + walkSpeed);
+        //Min
+        if(walkSpeed <= 1f)
+        {
+            walkSpeed = 1f;
+            
+        }    
     }
-
+       
     void FixedUpdate ()
     {
         //Move our Character
