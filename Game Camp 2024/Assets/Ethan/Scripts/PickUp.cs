7f145362c2f2c92591f6bbab094a61ceb56c2beb
EthanColon <ethanrcolon@gmail.com>
2024-07-16 18:04:30 -0500

    Doing door

--- Game Camp 2024/Assets/Ethan/Scripts/PickUp.cs
+++ Game Camp 2024/Assets/Ethan/Scripts/PickUp.cs
@@ -14,7 +14,8 @@ public enum interractables
 public class PickUp : MonoBehaviour
 {
     public interractables things = new interractables();
-
+    public GameObject NPCPrefab;
+    public GameObject NPCLocation;
     void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -24,25 +25,33 @@ public class PickUp : MonoBehaviour
                 case interractables.npc:
                     //Debug.Log("Collided with NPC");
                     GameObject.Find("NPCManager").GetComponent<NPCCounter>().NPCSaved();
+                    GameObject.Destroy(gameObject);
                     break;
                 case interractables.haystack:
                     //Debug.Log("Collided with haystack");
                     GameObject.Find("NPCManager").GetComponent<NPCCounter>().HaystackSaved();
+                    GameObject.Destroy(gameObject);
                     break;
                 case interractables.key:
                     //Debug.Log("Collided with key");
                     GameObject.Find("NPCManager").GetComponent<NPCCounter>().KeySaved();
+                    GameObject.Destroy(gameObject);
                     break;
                 case interractables.plank:
                     //Debug.Log("Collided with plank");
                     GameObject.Find("NPCManager").GetComponent<NPCCounter>().PlankSaved();
+                    GameObject.Destroy(gameObject);
                     break;
                 case interractables.door:
                     //Debug.Log("Collided with door");
-                    if(GameObject.Find("NPCManager").GetComponent<NPCCounter>().KeyCount >= 1);
+                    if(GameObject.Find("NPCManager").GetComponent<NPCCounter>().KeyCount >= 1)
+                    {
+                        GameObject.Find("NPCManager").GetComponent<NPCCounter>().KeyCount = GameObject.Find("NPCManager").GetComponent<NPCCounter>().KeyCount-1;
+                        GameObject.Destroy(gameObject);
+                        GameObject.Instantiate(NPCPrefab, NPCLocation.transform);
+                    }
                     break;
             };
-            GameObject.Destroy(gameObject);
         }
     }
 }
