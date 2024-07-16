645c24a82dcbf1f5b62dea221ff93f525214f3d6
EthanColon <ethanrcolon@gmail.com>
2024-07-16 17:33:05 -0500

    Added the NPCS saved text

--- Game Camp 2024/Assets/Ethan/Scripts/NPCCounter.cs
+++ Game Camp 2024/Assets/Ethan/Scripts/NPCCounter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
 
 public class NPCCounter : MonoBehaviour
 {
@@ -11,10 +12,12 @@ public class NPCCounter : MonoBehaviour
     int KeyCount = 0;
     int PlankCount = 0;
     int NeedleCount = 0;
+    public TextMeshProUGUI NPCText;
 
     // Start is called before the first frame update
     void Awake()
     {
+        NPCText.text = "NPCS:";
         if (instance == null)
         {
             instance = this;
@@ -31,13 +34,15 @@ public class NPCCounter : MonoBehaviour
     public void NPCSaved()
     {
         NPCCount++;
-        Debug.Log(NPCCount + " NPCs saved");
+        NPCText.text = (" NPCs: " + NPCCount);
+
     }
 
     public void HaystackSaved()
     {
         HaystackCount++;
         Debug.Log(HaystackCount + " Haystacks collected");
+        
     }
 
     public void KeySaved()
