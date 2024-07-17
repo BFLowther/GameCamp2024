02c613f2769ab1708c861be281fe1849ad03d1e4
Aldridge26 <aldridgewalters26@gmail.com>
2024-07-17 17:58:08 -0500

    Started Inventory Display

--- Game Camp 2024/Assets/Aldridge/inventorydisplay.cs
+++ Game Camp 2024/Assets/Aldridge/inventorydisplay.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class inventorydisplay : MonoBehaviour
 {
-    
+    public NPCCounter npcCounter;
+    public TMP_Text needleDisplay;
+    public TMP_Text keyDisplay;
+    public TMP_Text plankDisplay;
 
     // Update is called once per frame
     void Update()
     {
-        
+        keyDisplay.text = npcCounter.KeyCount.ToString();
+
     }
 }
