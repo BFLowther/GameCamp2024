0bd6f69e99fe006efcdaf4ea832791e053684f4f
EthanColon <ethanrcolon@gmail.com>
2024-07-19 18:42:59 -0500

    more stuff

--- Game Camp 2024/Assets/Ethan/Scripts/WindowBehaviour.cs
+++ Game Camp 2024/Assets/Ethan/Scripts/WindowBehaviour.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindowBehaviour : MonoBehaviour
+public class WindowBehavior : MonoBehaviour
 {
+    public Sprite open;
+    SpriteRenderer window;
     // Start is called before the first frame update
     void Start()
     {
-        
+        window = GetComponent<SpriteRenderer>();
     }
 
+
     // Update is called once per frame
-    void Update()
+    public void ChangeDoor()
     {
-        
+        window.sprite = open;
     }
 }
