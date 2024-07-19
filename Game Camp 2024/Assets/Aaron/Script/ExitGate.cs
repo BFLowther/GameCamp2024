38c125cf8e8d41fd0b278c0177710ec5f2e2ffb4
EthanColon <ethanrcolon@gmail.com>
2024-07-19 12:19:07 -0500

    Added leaderboard to the prefabs

--- Game Camp 2024/Assets/Aaron/Script/ExitGate.cs
+++ Game Camp 2024/Assets/Aaron/Script/ExitGate.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ExitGate : MonoBehaviour
 {
     public GameTimer gt;
+    public GameObject leaderboard;
+    public GameObject UI;
     public NPCCounter nc;
     public Leaderboard lb;
+
+    [System.Obsolete]
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("Player"))
         {
             nc.currentScore *= (int) gt.currentTime;
+            leaderboard.active = true;
+            UI.active = false;
             lb.AddHighScoreEntry((int)nc.currentScore, "JON");
             Debug.Log("game ended");
         }
