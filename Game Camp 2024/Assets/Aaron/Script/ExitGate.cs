617404d60de8eae3253fc007a5d254e324eb0285
EthanColon <ethanrcolon@gmail.com>
2024-07-19 18:19:38 -0500

    stuff

--- Game Camp 2024/Assets/Aaron/Script/ExitGate.cs
+++ Game Camp 2024/Assets/Aaron/Script/ExitGate.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExitGate : MonoBehaviour
 {
     public GameTimer gt;
     public GameObject leaderboard;
     public GameObject UI;
+    public int totalNPCs;
     public NPCCounter nc;
     public Leaderboard lb;
+    public GameObject player;
+    public Sprite[] expressions;
 
-    [System.Obsolete]
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.CompareTag("Player"))
         {
+            if (nc.currentScore == totalNPCs)
+                player.GetComponent<Image>().sprite = expressions[0];
+            else if (nc.currentScore >= totalNPCs-3)
+                player.GetComponent<Image>().sprite = expressions[1];
+            else if (nc.currentScore >= totalNPCs - 6)
+                player.GetComponent<Image>().sprite = expressions[2];
+            else
+                player.GetComponent<Image>().sprite = expressions[3];
             nc.currentScore *= (int) gt.currentTime;
-            leaderboard.active = true;
-            UI.active = false;
+            leaderboard.SetActive(true);
+            UI.SetActive(false);
             lb.AddHighScoreEntry((int)nc.currentScore, "JON");
             Debug.Log("game ended");
         }
