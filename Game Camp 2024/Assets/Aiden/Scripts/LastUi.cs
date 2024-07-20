7ec34c59a36c16e8aed600766beb71eb00029424
AidenThacker <aidenthacker86@gmail.com>
2024-07-19 19:58:54 -0500

    Worked on the Player Audio

--- Game Camp 2024/Assets/Aiden/Scripts/LastUi.cs
+++ Game Camp 2024/Assets/Aiden/Scripts/LastUi.cs
@@ -4,34 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LastUi : MonoBehaviour
-{
-    public Sprite[] Sprites;
-    public NPCCounter np;
-    
-    public void SpriteResult()
-    {
-        if(np.currentScore >= 8000)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites[0];
-        }
-        if(np.currentScore < 8000 && np.currentScore > 6000)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites[1];
-        }
-        if(np.currentScore < 6000 && np.currentScore > 4000)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites[3];
-        }
-        if(np.currentScore < 4000 && np.currentScore > 2000)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites[4];
-        }
-        if(np.currentScore < 2000 && np.currentScore > 0)
-        {
-            GetComponent<SpriteRenderer>().sprite = Sprites[5];
-        }
-    }
-    
+{   
     public void Retry()
     {
       Scene currentScene = SceneManager.GetActiveScene();
