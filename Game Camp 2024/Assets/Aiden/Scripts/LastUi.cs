e7a52ff734b9fbda83d5fd36a0855835e557fb81
Sky Wess <smwess5682@outlook.com>
2024-07-19 20:35:32 -0500

    Expressions fixed on the end screen

--- Game Camp 2024/Assets/Aiden/Scripts/LastUi.cs
+++ Game Camp 2024/Assets/Aiden/Scripts/LastUi.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LastUi : MonoBehaviour
-{   
+{
+    public int totalNPCs = 10;
+    public Leaderboard lb;
+    public GameObject player;
+    public Sprite[] expressions;
     public void Retry()
     {
       Scene currentScene = SceneManager.GetActiveScene();
@@ -13,5 +18,14 @@ public class LastUi : MonoBehaviour
     public void GiveUp()
     {
       SceneManager.LoadScene("MainMenu");
+        int savedNPC = PlayerPrefs.GetInt("NPCSaved", 0);
+        if (savedNPC == totalNPCs)
+            player.GetComponent<Image>().sprite = expressions[0];
+        else if (savedNPC >= totalNPCs - 5)
+            player.GetComponent<Image>().sprite = expressions[1];
+        else if (savedNPC >= totalNPCs - 10)
+            player.GetComponent<Image>().sprite = expressions[2];
+        else
+            player.GetComponent<Image>().sprite = expressions[3];
     }
 }
